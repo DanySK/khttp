1d3137377c54940044e49fdc33732610eceb17f9
Danilo Pianini <danilo.pianini@unibo.it>
2023-06-22 14:10:16 +0200

    build: fix the task names

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 var publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
 git push --force origin \${nextRelease.version}
-./gradlew releaseKotlinMavenOnMavenCentralNexus
+./gradlew uploadKotlinOSSRHToMavenCentralNexus releaseStagingRepositoryOnMavenCentral
 `
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
